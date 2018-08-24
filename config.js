28b8e74c3bcfdb75aedef128f02174bd92959493
Javier Lopez <lopezespinosjavier@gmail.com>
2018-08-24 18:27:58 +0200

    Changing Key names for heroku

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const twitterApp = {
-  consumer_key: process.env.CONSUMER_KEY,
-  consumer_secret: process.env.CONSUMER_SECRET,
+  consumer_key: process.env.API_KEY,
+  consumer_secret: process.env.API_SECRET_KEY,
   access_token: process.env.ACCESS_TOKEN,
   access_token_secret: process.env.ACCESS_TOKEN_SECRET,
   timeout_ms: 60 * 1000 // optional HTTP request timeout to apply to all requests.

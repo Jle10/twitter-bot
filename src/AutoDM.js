6191062d8247303d6d7495f7f6854e3cd8864220
Javier Lopez <lopezespinosjavier@gmail.com>
2018-08-25 11:43:14 +0200

    Debugging

--- src/AutoDM.js
+++ src/AutoDM.js
@@ -6,6 +6,7 @@ const AutoDM = () => {
   const stream = T.stream("user");
   console.log("Iniciado el proceso de envio automatico de Mensajes Directos 🚀🚀🚀");
   stream.on("follow", SendMessage);
+  console.log("stream lanzado 8=====D");
 };
 
 const SendMessage = user => {
